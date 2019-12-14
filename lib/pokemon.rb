738e8dbd5f6e8e40e9746600311a759bc3490b9d
Alex Velasquez <jtinnesco@gmail.com>
2019-12-14 03:40:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,10 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
 
   def intialize(id, name, type, db)
     @id = id
     @name = name
     @type = type
-    @db = db 
+    @db = db
   end
 end
